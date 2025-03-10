3dc440d1f5976a7186c1518160a6914a559e10d6
Assilbekov Didar <158120644+Didvr02@users.noreply.github.com>
2025-03-10 20:20:17 +0500

    Delete src/ChatAdapterDemo.java

--- src/ChatAdapterDemo.java
+++
@@ -1,9 +0,0 @@
-public class ChatAdapterDemo {
-    public static void main(String[] args) {
-        LegacyChatService legacyChatService = new LegacyChatService();
-
-        ChatService chatService = new ChatServiceAdapter(legacyChatService);
-
-        chatService.sendMessage("Hello world!");
-    }
-}
