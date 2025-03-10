1358dd7c427b11dce93acbf5fd084ab3fc5283ec
Didvr02 <didar.assilbekov@narxoz.kz>
2025-03-10 20:27:25 +0500

    Secondd commit

---
+++ src/Task2/ChatService.java
@@ -0,0 +1,5 @@
+package Task2;
+
+public interface ChatService {
+    void sendMessage(String message);
+}
---
+++ src/Task2/ChatServiceAdapter.java
@@ -0,0 +1,14 @@
+package Task2;
+
+public class ChatServiceAdapter implements ChatService {
+    private final LegacyChatService legacyChatService;
+
+    public ChatServiceAdapter(LegacyChatService legacyChatService) {
+        this.legacyChatService = legacyChatService;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        legacyChatService.sendLegacyMessage(message);
+    }
+}
